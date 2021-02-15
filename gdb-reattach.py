0959a79128672c68ce004bbe85c7a0b843bc4901
Steven Fosmark <sfosmark@calpoly.edu>
2021-02-15 15:49:32 -0800

    Debugged for use with pwndbg on Ubuntu 18 with python3

--- gdb-reattach.py
+++ gdb-reattach.py
@@ -1,4 +1,5 @@
 from __future__ import with_statement
+from subprocess import *
 import gdb
 
 class ReattachCommand (gdb.Command):
@@ -18,18 +19,20 @@ First argument is the name of executable (enough to specify the first time)"""
             self.lastFn = fn
             
         if len(self.lastFn) == 0:
-            print 'You have to specify the name of the process (for pidof) for the first time (it will be cached for later)'
+            print('You have to specify the name of the process (for pidof) for the first time (it will be cached for later)')
             return
 
+        pid = -1
         try:
             pid = check_output(["pidof",self.lastFn]).strip()
         except CalledProcessError as e:
             if e.returncode == 1:
-                print 'Process not found :('
+                print('Process not found :(')
+                return 1
             else:
                 raise e
 
-        gdb.execute('attach ' + pid)
+        gdb.execute('attach ' + pid.decode())
         gdb.execute('c')
 
 ReattachCommand()
